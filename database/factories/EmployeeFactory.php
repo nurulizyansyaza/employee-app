8d13e1499734c10b20775adf81df4367c9420595
Yang Edruce <nurulizyansyaza@gmail.com>
2026-05-26 07:28:32 +0800

    fix employee factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -18,14 +18,14 @@ class EmployeeFactory extends Factory
         $now = Carbon::now();
 
         return [
-            'name'        => substr($this->faker->name(), 0, 30),
-            'birthdate'   => $this->faker->dateTimeBetween('-60 years', '-16 years')->format('Y-m-d'),
-            'sex'         => $this->faker->boolean(),
-            'address'     => substr($this->faker->address(), 0, 200),
-            'currency'    => $this->faker->randomElement(['USD', 'EUR', 'GBP', 'JPY', 'IDR', 'SGD', 'MYR', 'INR', 'AUD', 'CNY']),
-            'salary'      => $this->faker->numberBetween(2_000, 80_000_000),
-            'nik'         => (string) $this->faker->unique()->numerify('##########'),
-            'is_active'   => $this->faker->boolean(85),
+            'name'        => substr(fake()->name(), 0, 30),
+            'birthdate'   => fake()->dateTimeBetween('-60 years', '-16 years')->format('Y-m-d'),
+            'sex'         => fake()->boolean(),
+            'address'     => substr(fake()->address(), 0, 200),
+            'currency'    => fake()->randomElement(['USD', 'EUR', 'GBP', 'JPY', 'IDR', 'SGD', 'MYR', 'INR', 'AUD', 'CNY']),
+            'salary'      => fake()->numberBetween(2_000, 80_000_000),
+            'nik'         => (string) fake()->unique()->numerify('##########'),
+            'is_active'   => fake()->boolean(85),
             'entry_date'  => $now,
             'update_date' => $now,
         ];
